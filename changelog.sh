763150a8d8e6ca2a86f53456646b0c39a95c7f82
macro6461 <mattcroak718@gmail.com>
2022-04-13 15:20:00 -0400

    first commit

---
+++ changelog.sh
@@ -0,0 +1,48 @@
+changelog=CHANGELOG.md
+version="1.2.3"
+# use below to correspond with your tagged version
+# version="$(git describe --long)"
+date="$(date '+%Y-%m-%d')"
+
+new_changelog()
+{
+  echo "# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+" > CHANGELOG.md
+}
+
+new_changelog_item()
+{   
+    item="## [$version] - $date"
+
+    if grep -Fxq "$item" CHANGELOG.md; then 
+        echo "Changelog item already exists for $item."
+    else
+        while read line; do
+            if [[ $line == "## [Unreleased]"* ]]; then
+                newvar=$(<<<"$line" sed 's/[].*[]/\\&/g')
+                echo $newvar
+                echo "LINE FOUND IN" CHANGELOG.md
+                sed -i "" "s/$newvar/## [Unreleased]\n\n$item\n### Added\n - ADD CHANGE HERE!/" CHANGELOG.md
+                return
+            fi
+        done < CHANGELOG.md
+    fi
+
+}
+
+init()
+{
+    if test -f "$changelog" ; then
+        new_changelog_item
+    else
+        new_changelog
+    fi
+}
+
+init
